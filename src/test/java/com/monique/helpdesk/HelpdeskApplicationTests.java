0af14936b04743c20540c1395a8cba86646dcde9
Moonipns <monique.dpnunes@gmail.com>
2023-04-16 20:41:09 -0300

    projeto iniciado

---
+++ src/test/java/com/monique/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monique.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
