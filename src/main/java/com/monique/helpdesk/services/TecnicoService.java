b8e64ddaa1a75920f24e1514b11e057eb34f996a
Moonipns <monique.dpnunes@gmail.com>
2023-04-23 20:26:27 -0300

    endpoint findById para Tecnico

---
+++ src/main/java/com/monique/helpdesk/services/TecnicoService.java
@@ -0,0 +1,22 @@
+package com.monique.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.monique.helpdesk.domain.Tecnico;
+import com.monique.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+	
+	@Autowired
+	private TecnicoRepository repository;
+	
+	public Tecnico findById(Integer id) {
+		Optional<Tecnico> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+
+}
