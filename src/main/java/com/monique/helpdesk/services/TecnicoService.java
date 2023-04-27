f3aa23f1e092ee20a9d00392dc75389c23ca8e4e
Moonipns <monique.dpnunes@gmail.com>
2023-04-27 19:04:52 -0300

    endpoint create para Tecnico

--- src/main/java/com/monique/helpdesk/services/TecnicoService.java
+++ src/main/java/com/monique/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.monique.helpdesk.domain.Tecnico;
+import com.monique.helpdesk.domain.dtos.TecnicoDTO;
 import com.monique.helpdesk.repositories.TecnicoRepository;
 import com.monique.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -24,6 +25,12 @@ public class TecnicoService {
 	public List<Tecnico> findAll() {
 		return repository.findAll();
 
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 
 }
