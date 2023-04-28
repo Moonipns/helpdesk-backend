eda67807291ae94bbea308f95d1768688d2fd42b
Moonipns <monique.dpnunes@gmail.com>
2023-04-28 19:48:12 -0300

    findById para chamado

---
+++ src/main/java/com/monique/helpdesk/services/ChamadoService.java
@@ -0,0 +1,24 @@
+package com.monique.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.monique.helpdesk.domain.Chamado;
+import com.monique.helpdesk.repositories.ChamadoRepository;
+import com.monique.helpdesk.services.exceptions.ObjectnotFoundException;
+
+
+@Service
+public class ChamadoService {
+	
+	@Autowired
+	private ChamadoRepository repository;
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID: " + id));
+	}
+
+}
