c0155214bf21dd8960a6d8f694c746c267efad41
Moonipns <monique.dpnunes@gmail.com>
2023-04-29 18:36:12 -0300

    Autorização com JWT

--- src/main/java/com/monique/helpdesk/config/SecurityConfig.java
+++ src/main/java/com/monique/helpdesk/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.monique.helpdesk.security.JWTAuthenticationFilter;
+import com.monique.helpdesk.security.JWTAuthorizationFilter;
 import com.monique.helpdesk.security.JWTUtil;
 
 
@@ -43,6 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http.cors().and().csrf().disable();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
